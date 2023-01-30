f8e3991fba9af07c27dc25c999a9a91bb8dca403
Thomas Stack <stackta@bc.edu>
2023-01-29 21:12:05 -0500

    Version 1.9

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,26 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "Namaste"
+    @State private var messageString = ""
     
     var body: some View {
         VStack {
             
             Spacer()
-            
-            Image(systemName: "speaker.wave.2", variableValue: 0.67)
-                .resizable()
-                .scaledToFit()
-                .symbolRenderingMode(.multicolor)
-                .padding()
-                .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
-                .cornerRadius(30)
-                .shadow(color: .gray, radius: 30, x: 20, y: 20)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 30)
-                        .stroke(.teal, lineWidth: 1)
-                )
-                .padding()
             
             Text(messageString)
                 .font(.largeTitle)
@@ -41,22 +27,20 @@ struct ContentView: View {
             
             Spacer()
             
-            HStack {
-                Button("Awesome") {
-                    messageString = "You Are Awesome!"
+            Button("Show Message") {
+                let message1 = "You Are Awesome!"
+                let message2 = "You Are Great!"
+                if messageString == message1 {
+                    messageString = message2
+                } else {
+                    messageString = message1
                 }
-                .buttonStyle(.borderedProminent)
-                
-                Spacer()
-                
-                
-                Button("Great") {
-                    messageString = "You Are Great!"
-                }
-                .buttonStyle(.borderedProminent)
             }
-            .padding()
+            .buttonStyle(.borderedProminent)
+            
         }
+        .padding()
+        
     }
 }
 
