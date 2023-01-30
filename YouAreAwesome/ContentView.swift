004d2bb6b57977b1b76b09413977ab976c0107a7
Thomas Stack <stackta@bc.edu>
2023-01-29 20:35:57 -0500

    Version 1.8

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,43 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = "When the Genius Bar needs help, they call you!"
+    @State private var messageString = "Namaste"
     
     var body: some View {
-            VStack {
-                    
-                    Spacer()
-                    
-                    Text(messageString)
-                        .font(.largeTitle)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.red)
-                        .minimumScaleFactor(0.5)
-                        .multilineTextAlignment(.center)
-                        .frame(height: 150)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                    
-                    Spacer()
-                    
-                    HStack {
-                        Button("Awesome") {
-                            messageString = "You Are Awesome!"
-                        }
-                        .buttonStyle(.borderedProminent)
-                        
-                        Spacer()
-                    
-                        
-                        Button("Great") {
-                            messageString = "You Are Great!"
-                        }
-                        .buttonStyle(.borderedProminent)
-                    }
-                    .padding()
+        VStack {
+            
+            Spacer()
+            
+            Image(systemName: "speaker.wave.2", variableValue: 0.67)
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
+                .padding()
+                .background(Color(hue: 0.521, saturation: 0.299, brightness: 0.958))
+                .cornerRadius(30)
+                .shadow(color: .gray, radius: 30, x: 20, y: 20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(.teal, lineWidth: 1)
+                )
+                .padding()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(.red)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                
+                Button("Great") {
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
             }
+            .padding()
         }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
